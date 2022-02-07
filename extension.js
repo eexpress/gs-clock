e06e5dfbcf6374914d16188f5481f1538d5945b9
eexpress <eexpress@163.com>
2022-02-07 21:56:09 +0800

    need coordinates of actor

--- extension.js
+++ extension.js
@@ -15,6 +15,7 @@ const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 
 const size = 400;
+	let DA;
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
@@ -26,14 +27,48 @@ class Indicator extends PanelMenu.Button {
 			style_class: 'system-status-icon',
 		}));
 
-		let item = new PopupMenu.PopupBaseMenuItem();
+		let item = new PopupMenu.PopupBaseMenuItem({reactive: false});
+		//~ let item = new PopupMenu.PopupBaseMenuItem();
 		const da = new St.DrawingArea({
 			width: size, height: size
 		});
 		da.connect("repaint", this.on_draw.bind(this));
+		//~ item.connect("button-press-event",this.click.bind(this));
+		//~ item.connect("motion-event",this.hover.bind(this));
+		//~ da.connect("motion-event",this.hover.bind(this));
+		//~ St.DrawingArea's signals are inherited from Clutter.Actor
 		item.actor.add_child(da);
+		DA = da;
+
+		this.clickId = global.stage.connect('button-release-event', this.click.bind(this));
+		this.hoverId = global.stage.connect("motion-event",this.hover.bind(this));
 
 		this.menu.addMenuItem(item);
+	}
+
+	pickactor(){
+		const [x, y] = global.get_pointer();
+		const a = global.stage.get_actor_at_pos(Clutter.PickMode.ALL, x, y);
+		lg(a);
+		return a;
+	};
+
+	hover(actor, event){
+		if(!this.menu.isOpen) return Clutter.EVENT_PROPAGATE;
+		//~ if(this.pickactor() != this) return Clutter.EVENT_PROPAGATE;
+		lg("hover: "+event.get_coords());
+		lg("hover: "+global.get_pointer());
+		return Clutter.EVENT_STOP;
+	}
+
+	click(actor, event){
+		if(!this.menu.isOpen) return Clutter.EVENT_PROPAGATE;
+		//~ if(this.pickactor() != this) return Clutter.EVENT_PROPAGATE;
+		//~ lg(this);	//[0x5638d3b86bc0 Gjs_cairo_eexpss_gmail_com_extension_Indicator.panel-button:first-child last-child active focus]
+		lg("click: "+event.get_coords());
+		lg("click: "+global.get_pointer());
+		lg("click: "+DA.get_position());	//rel to PopupBaseMenuItem
+		return Clutter.EVENT_STOP;
 	}
 
 	align_show(ctx, showtext){
@@ -57,10 +92,11 @@ class Indicator extends PanelMenu.Button {
 	};
 
 	on_draw(area){
-		const back_color="#CECECE";
+		const back_color="light gray";
 		const hand_color='black';
 		const MIN = size/10;
 		const MAX = size/2-size/12;
+
 
 		let ctx = area.get_context();
 		//~ ctx.selectFontFace("Sans Bold 27", Cairo.FontSlant.NORMAL, Cairo.FontWeight.NORMAL);
@@ -110,6 +146,7 @@ class Indicator extends PanelMenu.Button {
 		const d0 = new Date();
 		const h = d0.getHours();
 		const m = d0.getMinutes();
+		this.draw_line(ctx, "white", size/25, 0, -parseInt(size/4));	//闹铃，30度1小时
 		this.draw_line(ctx, hand_color, size/20, (h*30+m*30/60)*(Math.PI/180),-parseInt(size/3.7));	//时针，30度1小时
 		this.draw_line(ctx, hand_color, size/33, m*6*(Math.PI/180),-parseInt(size/2.7));	//分针，6度1分钟
 		this.setcolor(ctx, hand_color, 1);
@@ -118,6 +155,7 @@ class Indicator extends PanelMenu.Button {
 		this.setcolor(ctx, 'red', 1);
 		ctx.arc(0,0,size/33,0,2*Math.PI);
 		ctx.fill();
+		ctx.$dispose();	// 释放context，有用？
 	};
 
 	draw_line(ctx, color, width, angle, len){
@@ -129,7 +167,25 @@ class Indicator extends PanelMenu.Button {
 		this.setcolor(ctx, color, 1);	//指针颜色
 		ctx.setLineWidth (width);
 		ctx.moveTo (0, 0); ctx.lineTo(0, len); ctx.stroke();
+		if(color == "white"){
+			this.setcolor(ctx, 'red', 1);
+			ctx.arc(0,len,width/2*0.6,0,2*Math.PI);
+			ctx.fill();
+		}
 		ctx.restore();	//消除旋转的角度
+	}
+
+	destroy() {
+		if (this.hoverId != null) {
+			global.stage.disconnect(this.hoverId);
+			this.hoverId = null;
+		}
+		if (this.clickId != null) {
+			global.stage.disconnect(this.clickId);
+			this.clickId = null;
+		}
+		if (this._actor) this._actor.destroy();
+		super.destroy();
 	}
 
 });
