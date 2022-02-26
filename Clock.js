d3de22ea25adc511eed1b70866017f44def145f6
eexpress <eexpress@163.com>
2022-02-26 18:57:13 +0800

    Clock.js

--- Clock.js
+++ Clock.js
@@ -1,30 +1,147 @@
 const Cairo = imports.cairo;
-const { Clutter, Gio, GObject, GLib, Gtk } = imports.gi;
+const { Clutter, GObject, GLib, PangoCairo, Pango } = imports.gi;
 
 var Clock = GObject.registerClass({
 	Properties : {},
-	Signals : {
-		"child-hovered-event" : { param_types : [ GObject.TYPE_INT ] },
-	}
+	Signals : {}
 },
 class Clock extends Clutter.Actor {
-	_init(params = {}) {
-		super._init(params);
-		const canvas = new Clutter.Canvas({ height: 400, width: 400 });
-		canvas.connect('draw', (c, ctx, width, height) => {
-			ctx.setSourceRGB(0,255,0);
-			ctx.arc(128, 128, 76.8, 0, 45*Math.PI/180);
-			ctx.fill();
-		})
+	_init(size) {
+		super._init();
 
-		canvas.invalidate();
+		this.s = size;
+		this.degree;
+		this.alarm_degree = 0;
 
-		const actor = new Clutter.Actor();
-		actor.set_content(canvas);
-		actor.set_x_expand(true);
-		actor.set_y_expand(true);
+		this._canvas = new Clutter.Canvas();
+		this._canvas.connect('draw', this.on_draw.bind(this));
 
-		//~ return actor;
+		this._canvas.invalidate();
+		this._canvas.set_size(size, size);
+		this.set_size(size, size);
+		this.set_content(this._canvas);
 	}
 
+	draw_line(ctx, color, width, angle, len){
+		ctx.save();
+		ctx.rotate(angle);
+		//~ this.setcolor(ctx, shadow_color, 1);	//阴影
+		//~ ctx.setLineWidth (width+4);
+		//~ ctx.moveTo (0, 0); ctx.lineTo(0, len); ctx.stroke();
+		this.setcolor(ctx, color, 1);	//指针颜色
+		ctx.setLineWidth (width);
+		ctx.moveTo (0, 0); ctx.lineTo(0, len); ctx.stroke();
+		if(color == "white"){
+			this.setcolor(ctx, 'red', 1);
+			ctx.arc(0,len,width/2*0.6,0,2*Math.PI);
+			ctx.fill();
+		}
+		ctx.restore();	//消除旋转的角度
+	}
+
+	align_show(ctx, showtext){
+		// API没有绑定这个函数。 Cairo.TextExtents is not a constructor
+		//~ https://gitlab.gnome.org/GNOME/gjs/-/merge_requests/720
+		//~ let ex = new Cairo.TextExtents();
+		//~ ctx.textExtents (showtext, ex);
+		//~ ctx.relMoveTo(-ex.width/2,ex.height/2);
+		//~ ctx.showText(showtext);
+		let pl = PangoCairo.create_layout(ctx);
+		pl.set_text(showtext, -1);
+		pl.set_font_description(Pango.FontDescription.from_string("Sans Bold 20"));
+		PangoCairo.update_layout(ctx, pl);
+		let [w,h] = pl.get_pixel_size();
+		ctx.relMoveTo(-w/2,0);
+		PangoCairo.show_layout (ctx, pl);
+		ctx.relMoveTo(w/2,0);
+	}
+
+	setcolor(ctx, colorstr, alpha){
+		const [,cc] = Clutter.Color.from_string(colorstr);
+		ctx.setSourceRGBA(cc.red, cc.green, cc.blue, alpha);
+	}
+
+	on_draw(canvas, ctx, width, height){
+		const size = this.s;
+		const back_color="light gray";
+		const hand_color='black';
+		const MIN = size/10;
+		const MAX = size/2-size/12;
+
+		//~ let ctx = area.get_context();
+		//~ ctx.selectFontFace("Sans Bold 27", Cairo.FontSlant.NORMAL, Cairo.FontWeight.NORMAL);
+		//~ Seems all font class in cairo are disable.
+
+		ctx.translate(size/2, size/2);	//窗口中心为坐标原点。
+		ctx.setLineCap (Cairo.LineCap.ROUND);
+		ctx.setOperator (Cairo.Operator.SOURCE);
+
+		this.setcolor(ctx, back_color, 0.8);	//底色
+		ctx.arc(0,0,size/2-size/20,0,2*Math.PI);
+		ctx.fill();
+		ctx.setLineWidth(size/100);
+		this.setcolor(ctx, hand_color, 1);
+		ctx.arc(0,0,size/2-size/20,0,2*Math.PI);
+		ctx.stroke();
+		ctx.setLineWidth(size/200);
+		this.setcolor(ctx, 'white', 1);
+		ctx.arc(0,0,size/2-size/7.5,0,2*Math.PI);
+		ctx.stroke();
+
+		ctx.save();	//刻度
+		const scale = 60; let kp = false;
+		for(let i=0;i<scale;i++){
+			ctx.moveTo(0,-MAX);
+			if(i%5==0){
+				if(i%15==0){
+					ctx.setOperator(Cairo.Operator.SOURCE);
+					this.setcolor(ctx, hand_color, 1);
+					this.align_show(ctx, (i/5).toString());
+					ctx.setLineWidth(size/30);
+				}
+				else {
+					ctx.setOperator(Cairo.Operator.ATOP);
+					this.setcolor(ctx, back_color, 1);
+					ctx.setLineWidth(size/50);
+				}
+				ctx.relMoveTo(0,-size/35);
+				ctx.relLineTo(0,size/70);
+			}
+			ctx.stroke();
+			ctx.rotate((360/scale)*(Math.PI/180));	//6度一个刻度
+		}
+		ctx.restore();
+
+		const angle = this.degree*Math.PI/180;
+		if(angle){
+			//~ ctx.setOperator (Cairo.Operator.SOURCE);
+			this.setcolor(ctx, 'red', 1);	//hover 指示
+			ctx.rotate(-Math.PI/2);
+			ctx.setLineWidth (20);
+			ctx.moveTo(0,0);
+			ctx.arc(0,0,size/4,0,angle);
+			ctx.fill();
+			ctx.rotate(Math.PI/2);
+		}
+
+		const d0 = new Date();	//时间
+		const h = d0.getHours();
+		const m = d0.getMinutes();
+		this.draw_line(ctx, "white", size/25, this.alarm_degree*Math.PI/180, -Math.floor(size/4));	//闹铃，30度1小时
+		this.draw_line(ctx, hand_color, size/20, (h*30+m*30/60)*(Math.PI/180),-Math.floor(size/3.7));	//时针，30度1小时
+		this.draw_line(ctx, hand_color, size/33, m*6*(Math.PI/180),-Math.floor(size/2.7));	//分针，6度1分钟
+		this.setcolor(ctx, hand_color, 1);
+		ctx.arc(0,0,size/20,0,2*Math.PI);
+		ctx.fill();
+		this.setcolor(ctx, 'red', 1);
+		ctx.arc(0,0,size/33,0,2*Math.PI);
+		ctx.fill();
+		ctx.$dispose();	// 释放context，有用？
+	}
+
+	//~ on_draw(canvas, ctx, width, height){
+		//~ ctx.setSourceRGB(0,255,0);
+		//~ ctx.arc(28, 28, 76.8, 0, 45*Math.PI/180);
+		//~ ctx.fill();
+	//~ };
 });
--- extension.js
+++ extension.js
@@ -16,6 +16,10 @@ function lg(s) {
 	if (debug) log("===" + Me.metadata['gettext-domain'] + "===>" + s);
 }
 
+const Clock = Me.imports.Clock.Clock;
+
+const size = 400;
+
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
 	_init() {
@@ -26,33 +30,31 @@ class Indicator extends PanelMenu.Button {
 			style_class: 'system-status-icon',
 		}));
 
-//~ new Clutter.cc -> set_content -> new Clutter.Canvas -> ctx
-		const canvas = new Clutter.Canvas({ height: 400, width: 400 });
-		canvas.connect('draw', (c, ctx, width, height) => {
-			ctx.setSourceRGB(0,255,0);
-			ctx.arc(128, 128, 76.8, 0, 45*Math.PI/180);
-			ctx.fill();
-		})
+		this.cc = new Clock(300);
+		global.stage.add_child(this.cc);
+		//~ this.cc = new St.Bin({ height: size, width: size });
+		//~ this.cc = new Clutter.Actor({ height: size, width: size });
+		//~ this.cc.set_content(canvas);
+		//~ this.cc.set_x_expand(true);
+		//~ this.cc.set_y_expand(true);
 
-		canvas.invalidate();
-
-		this.cc = new Clutter.Actor();
-		this.cc.set_content(canvas);
-		this.cc.set_x_expand(true);
-		this.cc.set_y_expand(true);
-
-		Main.layoutManager.addChrome(this.cc);
+		//~ Main.layoutManager.addChrome(this.cc);
 		//~ global.stage.add_actor(this.cc);	//脱离面板的顶层透明显示。
-		this.cc.set_position(200,200);
-		this.cc.set_clip(0, 0, 400, 400);
-		this.cc.visible  = true;
-		this.cc.opacity = 255;
-		this.cc.reactive = true;
+		this.cc.set_position(100,100);
+		//~ this.cc.set_clip(0, 0, 400, 400);
+		//~ this.cc.visible  = true;
+		//~ this.cc.opacity = 255;
+		//~ this.cc.reactive = true;
+		//~ this.cc.queue_redraw();
 
 	}
 
 	destroy() {
-		Main.layoutManager.removeChrome(this.cc);
+		//~ Main.layoutManager.removeChrome(this.cc);
+		this.cc.destroy();
+		global.stage.remove_child(this.cc);
+		delete this.cc;
+		super.destroy();
 	}
 
 });
